7bee99ca8e4eb21c105c114e73819ab548f30436
yerowyn <yerowyn@gmail.com>
2020-04-08 02:01:00 +0600

    fix styles

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -10,10 +10,31 @@ const TaskList: React.FC<Props> = ({ tasks }) => {
         {tasks.map(task => {
           return (
             <li className="task-list-item" key={task.id}>
-              {task.title}
+              <div className="title">{task.title}</div>
             </li>
           );
         })}
+        <style jsx>{`
+          ul {
+            list-style: none;
+            margin: 0 0 30px;
+            padding: 0;
+          }
+
+          li {
+            align-items: center;
+            border: 1px solid #dde5ff;
+            display: flex;
+            padding: 14px;
+          }
+
+          li:nth-child(odd) {
+            background: #fcfdff;
+          }
+          .title {
+            margin: 0 20px;
+          }
+        `}</style>
       </ul>
     </>
   );
